9bf5f174d1d4da3a3d2de62e3b90e723eeb80b62
Josh Broton <joshbroton@gmail.com>
2015-09-23 14:00:09 -0500

    clean up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,50 @@
-var gulp = require('gulp'),
-    connect = require('gulp-connect'),
-    open = require('gulp-open'),
-    babel = require('gulp-babel'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    clean = require('gulp-clean'),
-    copy = require('gulp-copy'),
-    gulpIgnore = require('gulp-ignore'),
-    minifyCss = require('gulp-minify-css'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
+var gulp        = require('gulp'),
+    connect     = require('gulp-connect'),
+    open        = require('gulp-open'),
+    babel       = require('gulp-babel'),
+    sass        = require('gulp-sass'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    clean       = require('gulp-clean'),
+    minifyCss   = require('gulp-minify-css'),
+    uglify      = require('gulp-uglify'),
+    rename      = require('gulp-rename'),
     htmlreplace = require('gulp-html-replace'),
-    zip = require('gulp-zip');
+    zip         = require('gulp-zip');
+
+var roots = {
+    dev: './dev/',
+    build: './build/'
+};
 
 gulp.task('connect', function() {
     connect.server({
-        root: './dev',
+        root: roots.dev,
         livereload: true
     });
 });
 
 gulp.task('html', function() {
-    gulp.src('./dev/*.html')
+    gulp.src(roots.dev + '*.html')
         .pipe(connect.reload());
 });
 
 gulp.task('babel', function() {
-    gulp.src('./dev/js/**/*.es6')
+    gulp.src(roots.dev + 'js/**/*.es6')
         .pipe(babel())
-        .pipe(gulp.dest('./dev/js'))
+        .pipe(gulp.dest(roots.dev + '/js'))
         .pipe(connect.reload());
 });
 
 gulp.task('sass', function() {
-    gulp.src('./dev/scss/styles.scss')
+    gulp.src(roots.dev + 'scss/styles.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dev/css'));
+        .pipe(gulp.dest(roots.dev + 'css'));
 });
 
 gulp.task('css', function() {
-    gulp.src('./dev/css/*.css')
+    gulp.src(roots.dev + 'css/*.css')
         .pipe(connect.reload());
 });
 
@@ -51,30 +54,30 @@ gulp.task('open', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['./dev/**/*.html'], ['html']);
-    gulp.watch(['./dev/js/**/*.es6'], ['babel']);
-    gulp.watch(['./dev/scss/**/*.scss'], ['sass']);
-    gulp.watch(['./dev/css/**/*.css'], ['css']);
+    gulp.watch([roots.dev + '**/*.html'], ['html']);
+    gulp.watch([roots.dev + 'js/**/*.es6'], ['babel']);
+    gulp.watch([roots.dev + 'scss/**/*.scss'], ['sass']);
+    gulp.watch([roots.dev + 'css/**/*.css'], ['css']);
 });
 
 // Delete the build directory if it exists
 gulp.task('clean', function() {
-    return gulp.src('./build')
+    return gulp.src(roots.build)
                .pipe(clean());
 });
 
 gulp.task('clean-unminified', ['build-copy', 'minify-css', 'uglify'], function() {
     return gulp.src([
-                    './build/css/styles.css',
-                    './build/js/site/app.js'
+                    roots.build + 'css/styles.css',
+                    roots.build + 'js/site/app.js'
                ])
                .pipe(clean());
 });
 
 gulp.task('build-copy', ['clean'], function() {
     return gulp.src([
-                    './dev/**/*',
-                    '!./dev/{scss,scss/**}',
+                    roots.dev + '**/*',
+                    '!' + roots.dev + '{scss,scss/**}',
                     '!./**/*.map',
                     '!./**/*.es6'
                 ])
@@ -82,21 +85,21 @@ gulp.task('build-copy', ['clean'], function() {
 });
 
 gulp.task('minify-css', ['build-copy'], function() {
-    return gulp.src('./build/css/styles.css')
+    return gulp.src(roots.build + 'css/styles.css')
                .pipe(minifyCss())
                .pipe(rename({suffix: '.min'}))
                .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('uglify', ['build-copy'], function() {
-    return gulp.src('./build/js/site/**/*.js')
+    return gulp.src(roots.build + 'js/site/**/*.js')
                .pipe(uglify())
                .pipe(rename({suffix: '.min'}))
                .pipe(gulp.dest('build/js/site'));
 });
 
 gulp.task('html-replace', ['build-copy'], function() {
-    gulp.src('./build/index.html')
+    gulp.src(roots.build + 'index.html')
         .pipe(htmlreplace({
             'css': 'css/styles.min.css',
             'js': 'js/site/app.min.js'
@@ -108,7 +111,7 @@ gulp.task('zip', ['html-replace', 'clean-unminified'], function() {
     return gulp.src('./{build,build/**}')
                .pipe(zip('build.' + dateHash() + '.zip'))
                .pipe(gulp.dest('./'));
-})
+});
 
 gulp.task('server', ['sass', 'babel', 'connect', 'open', 'watch']);
 
